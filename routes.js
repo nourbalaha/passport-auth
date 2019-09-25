e88cd3181a60643a7c5545ec8fac6d0bbe8a1be7
Nour Balaha <nourbalaha0@gmail.com>
2019-09-26 04:35:23 +0900

    wip auth

--- routes.js
+++ routes.js
@@ -1,4 +1,52 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const User = require("./model");
+const passport = require("passport");
 
-module.exports = router;
+// main route
+router.get('/', (req, res) => {
+  res.render('index');
+})
+
+// login route
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+router.post('/login', passport.authenticate('local', { successRedirect: '/dashboard',
+                                                    failureRedirect: '/login' }));
+
+// register route
+router.get('/register', (req, res) => {
+  res.render('register');
+})
+
+router.post('/register', (req, res) => {
+    const {username, email, password } = req.body;
+    User.findOne({email})
+    .then(user=>{
+        if(!user){
+            const newUser = new User({username,email,password});
+            newUser.save()
+            .then(()=>{
+                res.render('dashboard');
+            })
+        } else {
+            res.send("email exists")
+        }
+    })
+    .catch();
+    
+})
+
+// dashboard route
+router.get('/dashboard', require("./auth").ensureAuthenticated, (req, res) => {
+  res.render('dashboard',{username: req.user.username});
+})
+
+// logout route
+router.get('/logout' ,(req, res) => {
+    req.logout();
+  res.redirect('/login');
+})
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const ejs = require("ejs");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+
 const app = express();
 
-app.set("view engine", "ejs");
+// passport config
+require("./passport")();
 
+// mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://nourbalaha:nour1988@cluster0-y65jq.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to database");
+});
+
+// parsers
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// express session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }))
+
+// passport 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// EJS
+app.set("view engine", "ejs");
 
 // Routes
 app.use("/",require("./routes"));
