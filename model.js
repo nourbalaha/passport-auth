ffc943dee7608b7b6949eb69ba33907a46bd36d5
Nour Balaha <nourbalaha0@gmail.com>
2019-09-26 04:34:05 +0900

    wip model

--- model.js
+++ model.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nourbalaha:nour1988@cluster0-y65jq.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("connected to database");
-});
 
 const UserSchema = new mongoose.Schema({
-    name: String
+    username: {type:String ,required: true},
+    email: {type:String ,required: true},
+    password: {type:String ,required: true},
   });
 
 const User = mongoose.model('User', UserSchema);
